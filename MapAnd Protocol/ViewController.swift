c593acab210a198d9a973846bd845e80b34b1990
shotaro-ooe <shoooe97@gmail.com>
2021-07-19 10:27:47 +0900

    緯度経度を取得してラベルに表示

--- MapAnd Protocol/ViewController.swift
+++ MapAnd Protocol/ViewController.swift
@@ -6,15 +6,82 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIGestureRecognizerDelegate,CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locManager:CLLocationManager!
+    
+    @IBOutlet weak var addresLabel: UILabel!
+    
+    @IBOutlet weak var settingBUtton: UIButton!
+    @IBOutlet var longPress: UILongPressGestureRecognizer!
+    
+    var addresssString = ""
     override func viewDidLoad() {
         super.viewDidLoad()
+        settingBUtton.backgroundColor = .white
+        settingBUtton.layer.cornerRadius = 20.0
+        
         
     }
 
+    @IBAction func longPressTap(_ sender: UILongPressGestureRecognizer) {
+        
+        
+        //タップを開始した
+        if sender.state == .began{
+            
+        }else if sender.state == .ended{
+            //タップを終了
+            //viewの中のlocationをtapPointに入れる
+            let tapPoint = sender.location(in: view)
+            
+            //タップした位置を指定して、MKMapvire上の緯度、経度を取得する
+            let center = mapView.convert(tapPoint, toCoordinateFrom:mapView)
+            let lat = center.latitude
+            let log = center.longitude
+            
+            //経度、緯度から住所に変換する
+            convert(lat: lat, log: log)
+            
+            
+            
+        }
+       
+    }
+    
+    //lat=緯度,log=経度を扱うクラス
+    func convert(lat:CLLocationDegrees,log:CLLocationDegrees){
+        
+        //クロージャーの中に書いてあるメンバ変数は、selfを書かないといけない。
+        //緯度、経度から住所を取得するときのお決まりの処理
+        let geocoder = CLGeocoder()
+        let location = CLLocation(latitude: lat, longitude: log)
+        
+        //クロージャー
+        //placeMark,errorに値が入ったら中の処理が呼ばれる。中の処理は後での処理になる。
+        geocoder.reverseGeocodeLocation(location) { (placeMark, error) in
+            if let placeMark = placeMark{
+                if let pm = placeMark.first{
+                    //locality=市、区
+                    if pm.administrativeArea != nil || pm.locality != nil {
+                        self.addresssString = pm.name! + pm.administrativeArea! + pm.locality!
+                    }else{
+                        self.addresssString = pm.name!
+                    }
+                    
+                    self.addresLabel.text = self.addresssString
+                }
+            }
+        }
+        
+        
+        
+    }
 
 }
-
