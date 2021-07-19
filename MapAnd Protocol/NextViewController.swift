e51d9e614f45c4a117ff59e5ce7d4d2d084d2ba6
shotaro-ooe <shoooe97@gmail.com>
2021-07-19 09:54:38 +0900

    UI完成

---
+++ MapAnd Protocol/NextViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NextViewController.swift
+//  MapAnd Protocol
+//
+//  Created by 大江祥太郎 on 2021/07/19.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
